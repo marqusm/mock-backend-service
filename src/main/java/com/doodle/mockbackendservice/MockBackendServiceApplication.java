8ce302612ff90b050845bfcd9fd015b61f04dc3d
misko <misko23@gmail.com>
2020-12-14 18:19:47 +0100

    Initial service implementation.

---
+++ src/main/java/com/doodle/mockbackendservice/MockBackendServiceApplication.java
@@ -0,0 +1,13 @@
+package com.doodle.mockbackendservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockBackendServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockBackendServiceApplication.class, args);
+	}
+
+}
